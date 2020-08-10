8d72f4645d83c72d15afaee7a2df781b952417e9
Ned Summers <ned@kwaia.com>
2020-08-10 11:47:56 +0100

    adding something

--- Scratches/Scratch.lean
+++ Scratches/Scratch.lean
@@ -1,10 +1,12 @@
-import init.data.nat.lemmas
-import init.algebra.order
-open nat 
+import category_theory.isomorphism
+open category_theory
+open category_theory.is_iso
 
-example (n : ℕ) (h : n < 1) : n = 0 :=
-begin
-cases h with  _ h,
-refl,
-cases h
-end
+universes v₁ v₂ v₃ u₁ u₂ u₃
+
+theorem twooutofthree {C : Type u₁} [category.{v₁} C] {X Y Z : C} (f : X ⟶ Y) (g : Y ⟶ Z) [is_iso f] [is_iso g] : is_iso (f ≫ g) :=
+    {
+        inv := is_iso.inv g ≫ is_iso.inv f,
+        hom_inv_id' := begin rw [category.assoc], rw [hom_inv_id' g] end,
+        inv_hom_id' := sorry
+    }
